25f73aeb1a1d9b0486aa9be56707c4b6d73d8fba
Phillip Bell <pbell24@students.kennesaw.edu>
2024-02-16 16:11:57 -0500

    Initial push 4 TestMyStack.java

--- src/Stack.java
+++ src/Stack.java
@@ -6,8 +6,8 @@
 // IDE Name: Inteli J
 
 //class is use to make a stack to store data
-public class Stack<e> {
-    public Node<e> head, tail;
+public class Stack<E> {
+    public Node<E> head, tail;
 
     // constructor method to create a list of objects with head, tail, and size.
     public Stack() {
@@ -16,7 +16,7 @@ public class Stack<e> {
     }
 
     // pushes data on stacks
-    public void addLastNode(e data) {
+    public void push(E data) {
         // if the list is empty
         if (tail == null)
             head = tail = new Node<>(data); // empty list
@@ -26,59 +26,17 @@ public class Stack<e> {
         }
     }
 
-    // method #1: add the first node
-    public void addFirstNode(e data) {
-        // if the list is empty
-        if (tail == null) {
-            head = tail = new Node<>(data); // empty list
-        } else {
-            Node<e> temp = new Node<>(data); // creates a temp node
-            temp.next = head; // links temp node to head
-            head = temp; // head is set to temp
-        }
-    }
-
-    // method #2: add node at index
-    public void addAtIndex(int index, e data) {
-        System.out.print("\nAdding value " + data + " at the index of " + index + ".\n" +
-                "List content before adding " + data + " is:\t");
-        printList();
-        Node<e> current = head;
-        Node<e> temp = head.next;
-        do {
-            temp = temp.next; // moves the node up the link list
-            current = current.next; // moves the node up the link list
-            index--;
-        } while (index != 1);
-
-        current.next = new Node<>(data); // inserts nodes
-        (current.next).next = temp; // attaches node to the next node
-    }
-
-    // method #3: remove the first node
-    public void removeFirstNode() {
+    // method #4: remove the node on the top of the stack
+    public void pop() {
         // if there are more than one node
         if (head.next != null) {
-            Node<e> temp = head; // creates a temp node = to head
-            head = temp.next; // links head to the next node
-            temp = null; // set the temp to null
-        } else { // if there is only 1 node, delete the list
-            head = null;
-            tail = null;
-        }
-    }
-
-    // method #4: remove the last node
-    public void removeLastNode() {
-        // if there are more than one node
-        if (head.next != null) {
-            Node<e> current = head; // creates a temp node = to head
+            Node<E> current = head; // creates a temp node = to head
             // iterates until at the second to last node
             while (current.next != tail) {
                 current = current.next;
             }
 
-            Node<e> temp = tail;
+            Node<E> temp = tail;
             tail = current; // links tail to the current node
             tail.next = null; // set the temp to null
         } else { // if there is only 1 node, delete the list
@@ -87,31 +45,15 @@ public class Stack<e> {
         }
     }
 
-    // method #5: remove node at index
-    public void removeAtIndex(int index) {
-        // if there are more than one node
-        if (head.next != null) {
-            Node<e> current = head; // creates a temp node = to head
-
-            // iterates until at the index
-            for (int i = 0; i < index + 1; i++) {
-                current = current.next;
-            }
-
-            Node<e> temp = current.next; // sets unwanted node to temp
-            current.next = current.next.next; // connect the two nodes to exclude the unwanted
-            temp = null; // discard unwanted node
-        } else {
-            // if there is only 1 node, delete the list
-            head = null;
-            tail = null;
-        }
+    // method #4: get the node on the top of the stack
+    public E top() {
+        return tail.data;//return the top of the stack
     }
 
-    // method #6: countNodes
-    public int countNodes() {
+    // counts the number of nodes in  the stack
+    public int size() {
         int listSize = 0;
-        Node<e> temp = head;
+        Node<E> temp = head;
 
         // iterate through nodes
         while (temp != null) {
@@ -123,8 +65,20 @@ public class Stack<e> {
         return listSize;
     }
 
+    //tell the user if the list is empty
+    public boolean isEmpty(){
+
+        if(head==null){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
     // method #7: printInReverse (Recursive method)
-    public String printInReverse(Node<e> L) {
+    public String printInReverse(Node<E> L) {
         String list = ""; // String variable that holds reverse
 
         // base case
@@ -141,7 +95,7 @@ public class Stack<e> {
 
     // method to print out the list
     public void printList() {
-        Node<e> temp;
+        Node<E> temp;
         temp = head;
         while (temp != null) {
             System.out.print(temp.data + "   ");
@@ -150,11 +104,11 @@ public class Stack<e> {
     }
 
     // class to create nodes as objects
-    private static class Node<e> {
-        private e data;  // data field
-        private Node<e> next; // link field
+    private static class Node<E> {
+        private E data;  // data field
+        private Node<E> next; // link field
 
-        public Node(e item) { // constructor method
+        public Node(E item) { // constructor method
             data = item;
             next = null;
         }
